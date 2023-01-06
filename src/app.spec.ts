d085e7fb9f8d37acce276a23a1266a81b47e7968
Felipe Schappo <felipeschappoo@gmail.com>
2023-01-06 18:06:48 -0300

    fix: notify

--- src/app.spec.ts
+++ src/app.spec.ts
@@ -3,7 +3,7 @@ import { sum } from './app';
 test('adds 1 + 2 to equal 3', () => {
   const a = 1;
   const b = 2;
-  expect(sum(a, b)).toBe(2);
+  expect(sum(a, b)).toBe(3);
 });
 
 test('adds 1 + 2 to equal 3', () => {
