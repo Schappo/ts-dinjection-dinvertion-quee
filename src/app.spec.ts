3d58df9ece9606e9454aa19b6be8b81a70b06c90
Felipe Schappo <felipeschappoo@gmail.com>
2023-01-06 01:38:08 -0300

    test: test

--- src/app.spec.ts
+++ src/app.spec.ts
@@ -5,3 +5,9 @@ test('adds 1 + 2 to equal 3', () => {
   const b = 2;
   expect(sum(a, b)).toBe(3);
 });
+
+test('adds 1 + 2 to equal 3', () => {
+  const a = 1;
+  const b = 2;
+  expect(sum(a, b)).toBe(3);
+});
